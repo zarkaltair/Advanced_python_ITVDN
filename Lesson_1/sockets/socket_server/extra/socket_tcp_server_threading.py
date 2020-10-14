b493e5552a25afb5fbc5b539c69099bc93102d48
Zark_Altair <zarkaltair@gmail.com>
2020-10-14 22:17:54 +0500

    socketserver threading

---
+++ Lesson_1/sockets/socket_server/extra/socket_tcp_server_threading.py
@@ -0,0 +1,19 @@
+import socketserver
+
+
+class ThreadingTCPServer(socketserver.ThreadingMixin, socketserver.TCPServer):
+    pass
+
+
+class EchoTCPHandler(socketserver.BaseRequestHandler):
+
+    def handle(self):
+        data = self.request.recv(1024).strip()
+        print(f'Addres: {self.client_address[0]}')
+        print(f'Data: {data.decode()}')
+        self.request.sendall(data)
+
+
+if __name__ == '__main__':
+    with socketserver.TCPServer(('', 8888), EchoTCPHandler) as server:
+        server.serve_forever()
