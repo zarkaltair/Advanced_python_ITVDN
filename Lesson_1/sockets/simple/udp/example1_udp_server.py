9a70af110ed672e32cd9ac8f970ff1fe809db4ac
Zark_Altair <zarkaltair@gmail.com>
2020-10-14 22:15:39 +0500

    socket udp

---
+++ Lesson_1/sockets/simple/udp/example1_udp_server.py
@@ -0,0 +1,9 @@
+# example 1 (UDP server socket)
+import socket
+
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind(('127.0.0.1', 8888))
+result = sock.recv(1024)
+print('Message', result.decode('utf-8'))
+sock.close()
